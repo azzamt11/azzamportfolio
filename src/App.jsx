bb96194b78bbf0d3bea0682cf3590e2128729ba7
abdullah azzam <dfeqhasavrymchaplctionnphysics@gmail.com>
2022-06-14 17:53:55 +0800

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,7 @@ function App() {
   }
 
   //matrices
-  const menuMatrix= ['home', 'about', 'works', 'contact'];
+  const menuMatrix= ['home', 'skills', 'works', 'contact'];
   const colorMatrix= ['#555555', '#f8c37d'];
   const secColorMatrix= ['white', '#f8c37d'];
 
@@ -54,7 +54,7 @@ function App() {
     document.getElementById('side-nav').style.cssText = 'left: 100vw';
     setP(0);
   }
-  const aboutFunction= ()=> {
+  const skillsFunction= ()=> {
     window.scrollTo({top: window.innerHeight+100, behavior: 'smooth'});
     document.getElementById('side-nav').style.cssText = 'left: 100vw';
     setP(0);
@@ -280,14 +280,14 @@ function App() {
     function restoreNameSection() {
       document.getElementById('name-section').style.cssText = 'left: 0px; opacity: 1;';
     }
-    function restoreAboutSection() {
-      document.getElementById('about-section').style.cssText = 'left: 0px; opacity: 1;';
+    function restoreskillsSection() {
+      document.getElementById('skills-section').style.cssText = 'left: 0px; opacity: 1;';
     }
-    function getRidAboutSectionToRight() {
-      document.getElementById('about-section').style.cssText = 'left: 100vw; opacity: 0;';
+    function getRidskillsSectionToRight() {
+      document.getElementById('skills-section').style.cssText = 'left: 100vw; opacity: 0;';
     }
-    function getRidAboutSectionToLeft() {
-      document.getElementById('about-section').style.cssText = 'left: -100vw; opacity: 0;';
+    function getRidskillsSectionToLeft() {
+      document.getElementById('skills-section').style.cssText = 'left: -100vw; opacity: 0;';
     }
     function restoreWorkSection() {
       document.getElementById('work-section').style.cssText = 'left: 0px; opacity: 1;';
@@ -314,13 +314,13 @@ function App() {
       var st = window.pageYOffset || document.documentElement.scrollTop;
       if (st > lastScrollTop && scrollParam < bottomSectionParam+1){
         scrollParam++;
-        if (window.scrollY> this.window.innerHeight && window.scrollY <= 3*this.window.innerHeight) {getRidNameSection(); restoreAboutSection(); colorMenu([0, 1, 0, 0]);}
-        else if (window.scrollY> 3*this.window.innerHeight && window.scrollY<= 5*this.window.innerHeight) {getRidAboutSectionToLeft(); restoreWorkSection(); colorMenu([0, 0, 1, 0]);}
+        if (window.scrollY> this.window.innerHeight && window.scrollY <= 3*this.window.innerHeight) {getRidNameSection(); restoreskillsSection(); colorMenu([0, 1, 0, 0]);}
+        else if (window.scrollY> 3*this.window.innerHeight && window.scrollY<= 5*this.window.innerHeight) {getRidskillsSectionToLeft(); restoreWorkSection(); colorMenu([0, 0, 1, 0]);}
         else if (window.scrollY> 5*this.window.innerHeight && window.scrollY<= 7*this.window.innerHeight) {getRidWorkSectionToLeft(); restoreContactSection(); colorMenu([0, 0, 0, 1]);}
       } else if (st < lastScrollTop && scrollParam >= topSectionParam) {
         scrollParam--;
-        if (window.scrollY< this.window.innerHeight) {restoreNameSection(); getRidAboutSectionToRight(); colorMenu([1, 0, 0, 0]);}
-        else if (window.scrollY < 3*this.window.innerHeight && window.scrollY>= this.window.innerHeight) {restoreAboutSection(); getRidWorkSectionToRight(); colorMenu([0, 1, 0, 0]);}
+        if (window.scrollY< this.window.innerHeight) {restoreNameSection(); getRidskillsSectionToRight(); colorMenu([1, 0, 0, 0]);}
+        else if (window.scrollY < 3*this.window.innerHeight && window.scrollY>= this.window.innerHeight) {restoreskillsSection(); getRidWorkSectionToRight(); colorMenu([0, 1, 0, 0]);}
         else if (window.scrollY < 5*this.window.innerHeight && window.scrollY>= 3*this.window.innerHeight) {restoreWorkSection(); getRidContactSectionToRight(); colorMenu([0, 0, 1, 0]);}
       }
       lastScrollTop = st <= 0 ? 0 : st;
@@ -338,7 +338,7 @@ function App() {
           <div id= 'logo'><div id= 'photo'></div><div id= 'name'>Abdullah Azzam</div></div>
           <div className= 'nav'>
             <div className= 'menu home' id= 'home' onClick= {homeFunction}>Home</div>
-            <div className= 'menu about' id= 'about' onClick= {aboutFunction}>Skills</div>
+            <div className= 'menu skills' id= 'skills' onClick= {skillsFunction}>Skills</div>
             <div className= 'menu works' id= 'works'onClick= {worksFunction}>Works</div>
             <div className= 'menu contact' id= 'contact'onClick= {contactFunction}>Contact</div>
           </div>
@@ -371,21 +371,30 @@ function App() {
         <div id= 'canvas1'></div>
         <div id= 'side-nav'>
           <div className= 'side-nav-menu center-flex' id= 'side-home' onClick= {homeFunction}>Home</div>
-          <div className= 'side-nav-menu center-flex' id= 'side-about'  onClick= {aboutFunction}>Skills</div>
+          <div className= 'side-nav-menu center-flex' id= 'side-skills'  onClick= {skillsFunction}>Skills</div>
           <div className= 'side-nav-menu center-flex' id= 'side-works'  onClick= {worksFunction}>Works</div>
           <div className= 'side-nav-menu center-flex last-menu' id= 'side-contact'  onClick= {contactFunction}>Contact</div>
         </div>
     </section>
-    <section id= 'about-section' className= 'section'>
+    <section id= 'skills-section' className= 'section'>
       <div className= 'left'>
         <span className= 'greeting0'>Skills</span>
-        <div id= 'about-greeting' className= 'greeting1'>Hello Everyone...</div>
-        <div id= 'about-me' className= 'greeting2'>I am Abdullah Azzam, I am a self-taught web and android developer, Here are some of my programming skills, including some languages and frameworks.</div>
-        <div id= 'about-canvas'></div>
+        <div id= 'skills-greeting' className= 'greeting1'>Hello Everyone...</div>
+        <div id= 'skills-me' className= 'greeting2'>I am Abdullah Azzam, I am a self-taught web and android developer, Here are some of my programming skills, including some languages and frameworks.</div>
+        <div id= 'skills-canvas'></div>
       </div>
-      <div id= 'right-about-section' className= 'right'>
-        <div id= 'canvas-field'>
-          
+      <div id= 'right-skills-section' className= 'right'>
+        <div id= 'skills-section-canvas'>
+          <div id= 'upper-skills-component'>
+            <div className= 'upper-skills-item'><div className= 'skills-image' id= 'skill-1'><div className= 'skill-persentage'><span className= 'span-01'>75%</span></div></div><div id= 'skills-text'>JavaScript</div></div>
+            <div className= 'upper-skills-item'><div className= 'skills-image' id= 'skill-2'><div className= 'skill-persentage'><span className= 'span-01'>70%</span></div></div><div id= 'skills-text'>Java</div></div>
+            <div className= 'upper-skills-item'><div className= 'skills-image' id= 'skill-3'><div className= 'skill-persentage'><span className= 'span-01'>70%</span></div></div><div id= 'skills-text'>PHP</div></div>
+          </div>
+          <div id= 'lower-skills-component'>
+            <div className= 'lower-skills-item'><div className= 'skills-image' id= 'skill-4'><div className= 'skill-persentage'><span className= 'span-01'>80%</span></div></div><div id= 'skills-text center-flex'>CSS</div></div>
+            <div className= 'lower-skills-item'><div className= 'skills-image' id= 'skill-5'><div className= 'skill-persentage'><span className= 'span-01'>75%</span></div></div><div id= 'skills-text center-flex'>React</div></div>
+            <div className= 'lower-skills-item'><div className= 'skills-image' id= 'skill-6'><div className= 'skill-persentage'><span className= 'span-01'>70%</span></div></div><div id= 'skills-text center-flex'>Flutter</div></div>
+          </div>
         </div>
       </div>
     </section> 
@@ -416,23 +425,25 @@ function App() {
       <div id= 'left-contact-section' className= 'left'>
       <div className= 'greeting0'>Contact</div>
         <div id= 'contact-greeting' className= 'greeting1'>Finally...</div>
-        <div id= 'contact-me' className= 'greeting2'>If you are impressed with this portfolio, or you are curious about what else I can do, feel free to contact or hire me.</div>
+        <div id= 'contact-me' className= 'greeting2'>If you are impressed with this portfolio, or you are curious skills what else I can do, feel free to contact or hire me.</div>
       </div>
       <div id= 'right-contact-section' className= 'right'>
         <div id= 'contact-section-canvas'>
-          <div className= 'contact-component'>
-            <div id= 'mail-greeting' className= 'contact-subcomponent'><span id= 'greeting-span'>You can send me anonymous message through this box if you want to give a comment or ask some question about me. The answer will be available <a href= 'http://www.example.com' id= 'ans-anchor' target= '_blank'>here</a>.</span></div>
-            <form id= 'message-field'>
-              <textarea id= 'mail-box' className= 'contact-subcomponent' onChange= {(e)=> {}}/>
-              <div id= 'mail-button' className= 'contact-button purple-button center-flex' value= 'Send' onClick ={(e)=> {mailButtonFunction()}}>Send</div>
-            </form>
-          </div>    
-          <div className= 'contact-component' id= 'hire-box'>
-            <div id= 'hire-greeting' className= 'contact-subcomponent'>If you are interrested to hire me. Please enter your whatsapp number and click the button below.</div>
-            <form id= 'hire-field'>
-              <input id= 'wa-number-input' placeholder= 'whatsapp-number'/>
-              <div id= 'hire-button' className='purple-button center-flex' type="submit" onClick ={(e)=> {hireButtonFunction()}}>Hire</div>
-            </form>
+          <div id= 'contact-sub-container'>
+            <div id= 'upper-contact-component'>
+              <div id= 'mail-greeting' className= 'contact-subcomponent'><span id= 'greeting-span'>You can send me anonymous message through this box if you want to give a comment or ask some question skills me. The answer will be available <a href= 'http://www.example.com' id= 'ans-anchor' target= '_blank'>here</a>.</span></div>
+                <form id= 'message-field'>
+                  <textarea id= 'mail-box' className= 'contact-subcomponent' onChange= {(e)=> {}}/>
+                  <div id= 'mail-button' className= 'contact-button purple-button center-flex' value= 'Send' onClick ={(e)=> {mailButtonFunction()}}>Send</div>
+                </form>
+              </div>    
+              <div id= 'lower-contact-component'>
+                <div id= 'hire-greeting' className= 'contact-subcomponent'>If you are interrested to hire me. Please enter your whatsapp number and click the button below.</div>
+                <form id= 'hire-field'>
+                  <input id= 'wa-number-input' placeholder= 'whatsapp-number'/>
+                  <div id= 'hire-button' className='purple-button center-flex' type="submit" onClick ={(e)=> {hireButtonFunction()}}>Hire</div>
+                </form>
+              </div>
           </div>
         </div>
       </div>
--- src/workcard.jsx
+++ src/workcard.jsx
@@ -5,6 +5,7 @@ function WorkCards() {
         <div id= 'work-cards'>
             <div className= 'card'>
                 <div className= 'box app-description-box'>
+                    <div className= 'app-display'></div>
                     <div className= 'app-name app' id= 'app-name-1'>Fictune</div>
                     <div className= 'app-description app' id= 'app-description-1'>Online Novel Application</div>
                     <div className= 'app-rate app' id= 'app-rate-1'></div>
@@ -13,6 +14,7 @@ function WorkCards() {
             </div>
             <div className= 'card'>
                 <div className= 'box app-description-box'>
+                    <div className= 'app-display'></div>
                     <div className= 'app-name app' id= 'app-name-2'>Nanum Olshop</div>
                     <div className= 'app-description app' id= 'app-description-2'>Online Shoping Application of Nanum</div>
                     <div className= 'app-rate app' id= 'app-rate-2'></div>
