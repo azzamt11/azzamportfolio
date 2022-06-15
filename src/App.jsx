45b64a09e4ef948f5901eb1974a36fe429b348e3
abdullah azzam <dfeqhasavrymchaplctionnphysics@gmail.com>
2022-06-16 06:36:30 +0800

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -358,7 +358,7 @@ function App() {
             </div>
             <div id= 'description'>
             <div id= 'div-job'>
-              <span id= 'span-job1'>Web Frontend Developer</span>
+              <span id= 'span-job1'>Web Fullstack Developer</span>
               <span id= 'span-job2'>Android Fullstack Developer</span>
             </div>
             <div id= 'social-media'>
@@ -387,13 +387,13 @@ function App() {
         <div id= 'skills-section-canvas'>
           <div id= 'upper-skills-component'>
             <div className= 'upper-skills-item'><div className= 'skills-image' id= 'skill-1'><div className= 'skill-persentage'><span className= 'span-01'>70%</span></div></div><div className= 'skills-text'>JavaScript</div></div>
-            <div className= 'upper-skills-item'><div className= 'skills-image' id= 'skill-2'><div className= 'skill-persentage'><span className= 'span-01'>60%</span></div></div><div className= 'skills-text'>Java</div></div>
-            <div className= 'upper-skills-item'><div className= 'skills-image' id= 'skill-3'><div className= 'skill-persentage'><span className= 'span-01'>70%</span></div></div><div className= 'skills-text'>Dart</div></div>
+            <div className= 'upper-skills-item'><div className= 'skills-image' id= 'skill-2'><div className= 'skill-persentage'><span className= 'span-01'>70%</span></div></div><div className= 'skills-text'>Dart</div></div>
+            <div className= 'upper-skills-item'><div className= 'skills-image' id= 'skill-3'><div className= 'skill-persentage'><span className= 'span-01'>70%</span></div></div><div className= 'skills-text'>PHP</div></div>
           </div>
           <div id= 'lower-skills-component'>
-            <div className= 'lower-skills-item'><div className= 'skills-image' id= 'skill-4'><div className= 'skill-persentage'><span className= 'span-01'>70%</span></div></div><div className= 'skills-text'>CSS</div></div>
-            <div className= 'lower-skills-item'><div className= 'skills-image' id= 'skill-5'><div className= 'skill-persentage'><span className= 'span-01'>70%</span></div></div><div className= 'skills-text'>React</div></div>
-            <div className= 'lower-skills-item'><div className= 'skills-image' id= 'skill-6'><div className= 'skill-persentage'><span className= 'span-01'>80%</span></div></div><div className= 'skills-text'>Flutter</div></div>
+            <div className= 'lower-skills-item'><div className= 'skills-image' id= 'skill-4'><div className= 'skill-persentage'><span className= 'span-01'>70%</span></div></div><div className= 'skills-text'>React</div></div>
+            <div className= 'lower-skills-item'><div className= 'skills-image' id= 'skill-5'><div className= 'skill-persentage'><span className= 'span-01'>80%</span></div></div><div className= 'skills-text'>Flutter</div></div>
+            <div className= 'lower-skills-item'><div className= 'skills-image' id= 'skill-6'><div className= 'skill-persentage'><span className= 'span-01'>80%</span></div></div><div className= 'skills-text'>Laravel</div></div>
           </div>
         </div>
       </div>
