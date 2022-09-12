bbad92d11f92bbc58f0863a0ba86258f10a58500
Abdullah Azzam <98200429+azzamt11@users.noreply.github.com>
2022-09-12 12:42:32 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -366,14 +366,14 @@ function App() {
       <div id= 'right-skills-section' className= 'right'>
         <div id= 'skills-section-canvas'>
           <div id= 'upper-skills-component'>
-            <div className= 'upper-skills-item'><div className= 'skills-image' id= 'skill-1'><div className= 'skill-persentage'><span className= 'span-01'>70%</span></div></div><div className= 'skills-text'>JavaScript</div></div>
+            <div className= 'upper-skills-item'><div className= 'skills-image' id= 'skill-1'><div className= 'skill-persentage'><span className= 'span-01'>80%</span></div></div><div className= 'skills-text'>JavaScript</div></div>
             <div className= 'upper-skills-item'><div className= 'skills-image' id= 'skill-2'><div className= 'skill-persentage'><span className= 'span-01'>70%</span></div></div><div className= 'skills-text'>Dart</div></div>
             <div className= 'upper-skills-item'><div className= 'skills-image' id= 'skill-3'><div className= 'skill-persentage'><span className= 'span-01'>70%</span></div></div><div className= 'skills-text'>PHP</div></div>
           </div>
           <div id= 'lower-skills-component'>
-            <div className= 'lower-skills-item'><div className= 'skills-image' id= 'skill-4'><div className= 'skill-persentage'><span className= 'span-01'>70%</span></div></div><div className= 'skills-text'>React</div></div>
+            <div className= 'lower-skills-item'><div className= 'skills-image' id= 'skill-4'><div className= 'skill-persentage'><span className= 'span-01'>80%</span></div></div><div className= 'skills-text'>React</div></div>
             <div className= 'lower-skills-item'><div className= 'skills-image' id= 'skill-5'><div className= 'skill-persentage'><span className= 'span-01'>80%</span></div></div><div className= 'skills-text'>Flutter</div></div>
-            <div className= 'lower-skills-item'><div className= 'skills-image' id= 'skill-6'><div className= 'skill-persentage'><span className= 'span-01'>80%</span></div></div><div className= 'skills-text'>Laravel</div></div>
+            <div className= 'lower-skills-item'><div className= 'skills-image' id= 'skill-6'><div className= 'skill-persentage'><span className= 'span-01'>70%</span></div></div><div className= 'skills-text'>Laravel</div></div>
           </div>
         </div>
       </div>
