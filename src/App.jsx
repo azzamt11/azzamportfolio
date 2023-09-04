134852b7a3ba272e44f04c5e8aff2d393de45b36
azzamt11 <abdullahazzamt11@gmail.com>
2023-09-04 18:08:30 +0700

    new feature

--- src/App.jsx
+++ src/App.jsx
@@ -83,7 +83,7 @@ function App() {
   //updown button function
   var cardPositionParam= 1;
   const buttonDownFunction= () => {
-    if (cardPositionParam>0 && cardPositionParam<2) {
+    if (cardPositionParam>-1 && cardPositionParam<3) {
       cardPositionParam--;
       console.log(cardPositionParam);
       document.getElementById('work-cards').style.cssText = `top: ${cardPositionParam*100}vh`;
@@ -98,7 +98,7 @@ function App() {
     }
   }
   const buttonUpFunction= () => {
-    if (cardPositionParam<1 && cardPositionParam>-1) {
+    if (cardPositionParam<1 && cardPositionParam>-2) {
       cardPositionParam++;
       console.log(cardPositionParam);
       document.getElementById('work-cards').style.cssText = `top: ${cardPositionParam*100}vh`;
@@ -366,14 +366,14 @@ function App() {
       <div id= 'right-skills-section' className= 'right'>
         <div id= 'skills-section-canvas'>
           <div id= 'upper-skills-component'>
-            <div className= 'upper-skills-item'><div className= 'skills-image' id= 'skill-1'><div className= 'skill-persentage'><span className= 'span-01'>80%</span></div></div><div className= 'skills-text'>JavaScript</div></div>
-            <div className= 'upper-skills-item'><div className= 'skills-image' id= 'skill-2'><div className= 'skill-persentage'><span className= 'span-01'>70%</span></div></div><div className= 'skills-text'>Dart</div></div>
-            <div className= 'upper-skills-item'><div className= 'skills-image' id= 'skill-3'><div className= 'skill-persentage'><span className= 'span-01'>70%</span></div></div><div className= 'skills-text'>PHP</div></div>
+            <div className= 'upper-skills-item'><div className= 'skills-image' id= 'skill-1'><div className= 'skill-persentage'><span className= 'span-01'>65%</span></div></div><div className= 'skills-text'>JavaScript</div></div>
+            <div className= 'upper-skills-item'><div className= 'skills-image' id= 'skill-2'><div className= 'skill-persentage'><span className= 'span-01'>80%</span></div></div><div className= 'skills-text'>Dart</div></div>
+            <div className= 'upper-skills-item'><div className= 'skills-image' id= 'skill-3'><div className= 'skill-persentage'><span className= 'span-01'>50%</span></div></div><div className= 'skills-text'>PHP</div></div>
           </div>
           <div id= 'lower-skills-component'>
-            <div className= 'lower-skills-item'><div className= 'skills-image' id= 'skill-4'><div className= 'skill-persentage'><span className= 'span-01'>80%</span></div></div><div className= 'skills-text'>React</div></div>
+            <div className= 'lower-skills-item'><div className= 'skills-image' id= 'skill-4'><div className= 'skill-persentage'><span className= 'span-01'>60%</span></div></div><div className= 'skills-text'>React</div></div>
             <div className= 'lower-skills-item'><div className= 'skills-image' id= 'skill-5'><div className= 'skill-persentage'><span className= 'span-01'>80%</span></div></div><div className= 'skills-text'>Flutter</div></div>
-            <div className= 'lower-skills-item'><div className= 'skills-image' id= 'skill-6'><div className= 'skill-persentage'><span className= 'span-01'>70%</span></div></div><div className= 'skills-text'>Laravel</div></div>
+            <div className= 'lower-skills-item'><div className= 'skills-image' id= 'skill-6'><div className= 'skill-persentage'><span className= 'span-01'>50%</span></div></div><div className= 'skills-text'>Laravel</div></div>
           </div>
         </div>
       </div>
--- src/workcard.jsx
+++ src/workcard.jsx
@@ -5,22 +5,33 @@ function WorkCards() {
         <div id= 'work-cards'>
             <div className= 'card'>
                 <div className= 'box app-description-box'>
-                    <div className= 'app-name app' id= 'app-name-1'>Fictune Web App</div>
-                    <div className= 'app-description app' id= 'app-description-1'>Online Novel Application</div>
-                    <div className= 'app-description app'>Role: Fullstack Developer,UI/UX Designer, Owner</div>
-                    <div className= 'app-description app'>Frontend Framework: React.js</div>
-                    <div className= 'app-description app'>Backend Framework: Laravel</div>
-                    <div className= 'app-description app'>Click image below to visit.</div>
-                    <a href= 'https://grand-gingersnap-348041.netlify.app' target="_blank"><div className= 'app-link app center-flex' id= 'app-link-1'></div></a>
+                    <div className= 'app-name app' id= 'app-name-1'>QuantumBook Mobile</div>
+                    <a href= 'https://play.google.com/store/apps/details?id=id.quantumbook&hl=en_US&pli=1' target="_blank"><div className= 'app-link app center-flex' id= 'app-link-1'></div></a>
+                    <div className= 'app-description app' id= 'app-description-1'>Book Catalog Mobile Application for QuantumBook Company</div>
+                    <div className= 'app-description app'>Role: Mobile Developer</div>
+                    <div className= 'app-description app'>Frontend Framework: Flutter</div>
+                    <div className= 'app-description app'>Click image to visit.</div>
                 </div>
             </div>
             <div className= 'card'>
                 <div className= 'box app-description-box'>
-                    <div className= 'app-display'></div>
-                    <div className= 'app-name app' id= 'app-name-2'>Fictune Mobile App</div>
-                    <div className= 'app-description app' id= 'app-description-2'>Mobile Novel Application</div>
-                    <div className= 'app-description app'>Role: Fullstack Developer,UI/UX Designer, Owner</div>
-                    <a href= 'http://azzamsblogdomain.42web.io/fictuneprogress' target="_blank"><div className= 'app-link app  center-flex' id= 'app-link-2'>View Progress</div></a>
+                    <div className= 'app-name app' id= 'app-name-2'>Tennis Game Application</div>
+                    <a href= 'https://azzamtennisgamewebversion1.netlify.app/#/' target="_blank"><div className= 'app-link app  center-flex' id= 'app-link-2'></div></a>
+                    <div className= 'app-description app' id= 'app-description-2'>Web Application for Azzam's Special Tennis Game</div>
+                    <div className= 'app-description app'>Role: Fullstack Developer, Owner</div>
+                    <div className= 'app-description app'>Frameworks: Flutter with Firebase</div>
+                    <div className= 'app-description app'>Click image to visit.</div>
+                </div>
+            </div>
+            <div className= 'card'>
+                <div className= 'box app-description-box'>
+                    <div className= 'app-name app' id= 'app-name-3'>Traveloka Mobile Clone</div>
+                    <a href= 'https://azzamtmcweb.netlify.app/#/' target="_blank"><div className= 'app-link app  center-flex' id= 'app-link-3'></div></a>
+                    <div className= 'app-description app' id= 'app-description-3'>Traveloka Mobile Application Clone</div>
+                    <div className= 'app-description app'>Role: Fullstack Developer</div>
+                    <div className= 'app-description app'>Frameworks: Flutter</div>
+                    <div className= 'app-description app'>Github Repo: <a href= "https://github.com/azzamt11/azzam-tmc">azzam-tmc</a></div>
+                    <div className= 'app-description app'>Click image to visit.</div>
                 </div>
             </div>
         </div>
